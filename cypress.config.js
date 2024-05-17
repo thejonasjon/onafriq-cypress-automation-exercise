90b069641bc710b8253429e7b1d6934c31c4ff2f
jonazy <jonas.humenu@gmail.com>
2024-05-17 20:19:09 +0100

    setup config, project structure

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,15 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'https://www.automationexercise.com/',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    defaultCommandTimeout: 10000,
+      requestTimeout: 5000,
+      pageLoadTimeout: 30000,
+    retries: {
+      runMode: 2,
+      openMode: 1,
+    },
   },
 });
